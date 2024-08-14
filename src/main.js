a11aad0106259575e7e936be78e7ff57ffd5ce6a
Andrey <andreidenisov155@gmail.com>
2024-08-15 00:07:39 +0400

    add speed insights

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import './assets/main.css'
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { createApp } from 'vue'
 import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
 import router from './router'
